c64254edca0f554e331c41e7da5ac8f2bc3007eb
J-Rios <jrios.rubio23@gmail.com>
2016-04-16 00:09:45 +0200

    Echo example added
    Echo example that return any received data from the Serial

---
+++ DigisparkSoftSerial_INT0/examples/Echo/Echo.ino
@@ -0,0 +1,20 @@
+#include <SoftSerial_INT0.h>
+
+#define P_RX 2                        // Reception PIN (SoftSerial)
+#define P_TX 1                        // Transmition PIN (SoftSerial)
+
+SoftSerial Bluetooth(P_RX, P_TX);           // Software serial port for control the Bluetooth module
+
+void setup()
+{
+    Bluetooth.begin(9600); // Initialize the serial port
+}
+
+void loop()
+{
+    // Basic Echo Test
+    if(Bluetooth.available())
+        Bluetooth.write(Ser.read());
+
+    delay(100);
+}
